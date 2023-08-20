70ecce8b05f86fa4a83bf750b20930bcc45988e1
TKapel <37240507+TKapel@users.noreply.github.com>
2023-08-21 00:02:45 +0200

    Update streamlitt_app.py

--- streamlitt_app.py
+++ streamlitt_app.py
@@ -34,7 +34,7 @@ def get_fruityvice_data(this_fruit_choice):
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information!")
   else:
@@ -43,7 +43,10 @@ try:
 except URLError as e:
     streamlit.error()
 
+#Don't run anything past here until code is fixed
+
 streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
@@ -51,5 +54,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+#This will not work correctly, but just go with it for now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
